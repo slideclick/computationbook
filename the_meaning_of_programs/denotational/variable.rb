224002a9675ad6f21e25185573351f789584724f
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-03-24 16:37:10 +0800

    how to eval to_py

--- the_meaning_of_programs/denotational/variable.rb
+++ the_meaning_of_programs/denotational/variable.rb
@@ -10,6 +10,7 @@ class Variable
     "function (e) { return e[#{ExecJS::JSON.encode(name)}]; }"
   end
   # expression = Add.new(Number.new(3), Number.new(1))
+  # copy the output to iPython, eval(string), then
   # _({'x':2})
   # expression = Add.new(Variable.new(:x), Number.new(1))
 
